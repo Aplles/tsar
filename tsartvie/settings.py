86bfd86644c5a21b96acfad0bc79f64f754cc7d9
Savva <imperiya66.ru@gmail.com>
2023-11-28 23:23:51 +0300

    поменяли пароль снова2 для почты

--- tsartvie/settings.py
+++ tsartvie/settings.py
@@ -135,8 +135,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # EMAIL_HOST_USER = 'test.pytohon@gmail.com'
 # EMAIL_HOST_PASSWORD = 'omcfrpqavbecfvxg'
 
-EMAIL_HOST = 'smtp.ht-systems.ru'
-EMAIL_PORT = 2525
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 DEFAULT_FROM_EMAIL = 'tsarstvie.ru@gmail.com'
